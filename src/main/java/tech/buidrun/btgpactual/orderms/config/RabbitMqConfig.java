43c711d22f3f6d85883bd2894a855fbd46671138
Ulisses Alves <ulisalves@gmail.com>
2026-01-16 12:30:54 -0300

    criação das classes entity para mapeamento no mongodb compass

--- src/main/java/tech/buidrun/btgpactual/orderms/config/RabbitMqConfig.java
+++ src/main/java/tech/buidrun/btgpactual/orderms/config/RabbitMqConfig.java
@@ -1,4 +1,9 @@
 package tech.buidrun.btgpactual.orderms.config;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RabbitMqConfig {
+
+    public static final String ORDER_CREATED_QUEUE = "btg-pactual-order-created";
 }
